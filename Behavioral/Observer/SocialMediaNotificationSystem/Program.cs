00c02211215cf079ea63be8233642a10204ad4b4
sondoan85 <thanhson2020@yahoo.com>
2024-12-04 00:23:49 +0700

    Examples of Observer.

--- Behavioral/Observer/SocialMediaNotificationSystem/Program.cs
+++ Behavioral/Observer/SocialMediaNotificationSystem/Program.cs
@@ -1,10 +1,86 @@
-﻿namespace SocialMediaNotificationSystem
+﻿using System.Xml.Linq;
+
+namespace SocialMediaNotificationSystem
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public interface IFollower
         {
-            Console.WriteLine("Hello, World!");
+            void Update(User user, string message);
+        }
+
+        public interface IUser
+        {
+            void Follow(IFollower follower);
+            void Unfollow(IFollower follower);
+            void NotifyFollowers(string message);
+        }
+
+        public class Follower : IFollower
+        {
+            public string Name { get; private set; }
+
+            public Follower(string name)
+            {
+                Name = name;
+            }
+
+            public void Update(User user, string message)
+            {
+                Console.WriteLine($"{Name} received a new post from {user.Name}: {message}");
+            }
+        }
+
+        public class User : IUser
+        {
+            public string Name { get; private set; }
+            private List<IFollower> _followers = new List<IFollower>();
+            public User(string name)
+            {
+                Name = name;
+            }
+
+            public void Follow(IFollower follower)
+            {
+                _followers.Add(follower);
+            }
+
+            public void Unfollow(IFollower follower)
+            {
+                _followers.Remove(follower);
+            }
+
+            public void PostMessage(string message)
+            {
+                Console.WriteLine($"{Name} posted: {message}");
+                NotifyFollowers(message);
+            }
+
+            public void NotifyFollowers(string message)
+            {
+                foreach (var follower in _followers)
+                {
+                    follower.Update(this, message);
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            User Pranaya = new User("Pranaya");
+            User Rout = new User("Rout");
+
+            Follower Anurag = new Follower("Anurag");
+            Follower Mohanty = new Follower("Mohanty");
+
+            Pranaya.Follow(Anurag); // Anurag follows Pranaya
+            Rout.Follow(Anurag);   // Anurag follows Rout
+            Pranaya.Follow(Mohanty);   // Mohanty follows Pranaya
+
+            Pranaya.PostMessage("Hello from Pranaya!");
+            Rout.PostMessage("Rout's first post.");
+
+            Console.ReadKey();
         }
     }
 }
--- Behavioral/Observer/StockMarketTrackingSystem/Program.cs
+++ Behavioral/Observer/StockMarketTrackingSystem/Program.cs
@@ -1,10 +1,103 @@
 ﻿namespace StockMarketTrackingSystem
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Stock HSBLStock = new Stock("HSBL", 150.00);
+            Trader Pranaya = new Trader("Pranaya");
+            Trader Kumar = new Trader("Kumar");
+            Trader Rout = new Trader("Rout");
+
+            //Register Traders
+            HSBLStock.RegisterTrader(Pranaya);
+            HSBLStock.RegisterTrader(Kumar);
+            HSBLStock.RegisterTrader(Rout);
+
+            //UnRegister
+            HSBLStock.UnregisterTrader(Kumar);
+
+            // Simulate a price change.
+            HSBLStock.Price = 152.50;
+            HSBLStock.Price = 149.50;
+            HSBLStock.Price = 155.50;
+
+            Console.ReadKey();
+        }
+
+        public interface ITrader
+        {
+            void Update(Stock stock, double oldPrice);
+        }
+
+        // Subject Interface
+        public interface IStockTicker
+        {
+            void RegisterTrader(ITrader trader);
+            void UnregisterTrader(ITrader trader);
+            void Notify(double oldPrice);
+        }
+
+        public class Trader : ITrader
+        {
+            public string Name { get; set; }
+
+            public Trader(string name)
+            {
+                Name = name;
+            }
+
+            public void Update(Stock stock, double oldPrice)
+            {
+                Console.WriteLine($"Notifying {Name} of {stock.Symbol}'s price change from ${oldPrice} to ${stock.Price}");
+            }
+        }
+
+        public class Stock : IStockTicker
+        {
+            private List<ITrader> _traders = new List<ITrader>();
+            
+            private double _price;
+
+            public string Symbol { get; private set; }
+
+            public double Price
+            {
+                get { return _price; }
+                set
+                {
+                    if (_price != value)
+                    {
+                        var oldPrice = _price;
+                        _price = value;
+                        Notify(oldPrice);
+                    }
+                }
+            }
+
+            public Stock(string symbol, double price)
+            {
+                Symbol = symbol;
+                Price = price;
+            }
+
+            public void RegisterTrader(ITrader trader)
+            {
+                _traders.Add(trader);
+            }
+
+            public void UnregisterTrader(ITrader trader)
+            {
+                _traders.Remove(trader);
+            }
+
+            public void Notify(double oldPrice)
+            {
+                foreach (var trader in _traders)
+                {
+                    trader.Update(this, oldPrice);
+                }
+            }
         }
     }
 }
