f474dd1a149f0340481acd31dc3570d9f6431b55
sondoan85 <thanhson2020@yahoo.com>
2024-12-03 18:13:44 +0700

    Add project files.

---
+++ Creational/Factory Method/LogisticsApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogisticsApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Creational/Prototype/CarConfiguratorApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CarConfiguratorApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
