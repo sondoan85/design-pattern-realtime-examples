9f240cfa0f3b490e627fdc31f396768a9f0c6f55
sondoan85 <thanhson2020@yahoo.com>
2024-12-03 20:06:36 +0700

    Examples for Factory Method.

--- Creational/Factory Method/LogisticsApplication/Program.cs
+++ Creational/Factory Method/LogisticsApplication/Program.cs
@@ -1,10 +1,65 @@
 ﻿namespace LogisticsApplication
 {
+    public interface ITransport
+    {
+        double GetDeliveryCost(int distance);    
+    }
+
+    public class Truck : ITransport
+    {
+        public double GetDeliveryCost(int distance)
+        {
+            // Cost per mile for the truck might be $1.00
+            return 1.00 * distance;
+        }
+    }
+
+    public class Ship : ITransport
+    {
+        public double GetDeliveryCost(int distance)
+        {
+            // Cost per mile for the ship might be $0.50
+            return 0.50 * distance;
+        }
+    }
+
+    public abstract class TransportFactory
+    {
+        public abstract ITransport CreateTransport();
+    }
+
+    public class TruckFactory : TransportFactory
+    {
+        public override ITransport CreateTransport()
+        {
+            return new Truck();
+        }
+    }
+
+    public class ShipFactory : TransportFactory
+    {
+        public override ITransport CreateTransport()
+        {
+            return new Ship();
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TransportFactory factory;
+            ITransport transport;
+
+            factory = new TruckFactory();
+            transport = factory.CreateTransport();
+            Console.WriteLine($"Truck delivery cost: ${transport.GetDeliveryCost(100)} for 100 miles.");
+
+            factory = new ShipFactory();
+            transport = factory.CreateTransport();
+            Console.WriteLine($"Ship delivery cost: ${transport.GetDeliveryCost(100)} for 100 miles.");
+
+            Console.ReadKey();
         }
     }
 }
--- Creational/Factory Method/PaymentGatewayIntegration/Program.cs
+++ Creational/Factory Method/PaymentGatewayIntegration/Program.cs
@@ -1,10 +1,86 @@
 ﻿namespace PaymentGatewayIntegration
 {
+    public interface IPaymentGateway
+    {
+        void ProcessPayment(decimal amount);
+    }
+
+    public class CreditCardPaymentGateway : IPaymentGateway
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing Credit card payment of amount: ${amount}");
+        }
+    }
+
+    public class PaypalPaymentGateway : IPaymentGateway
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing Paypal payment of amount: ${amount}");
+        }
+    }
+
+    public class BitcoinPaymentGateway : IPaymentGateway
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing Bitcoin payment of amount: ${amount}");
+        }
+    }
+
+    public abstract class PaymentGatewayFactory
+    {
+        public abstract IPaymentGateway CreatePaymentGateway();
+    }
+
+    public class CreditCardPaymentGatewayFactory : PaymentGatewayFactory
+    {
+        public override IPaymentGateway CreatePaymentGateway()
+        {
+            return new CreditCardPaymentGateway();
+        }
+    }
+
+    public class PaypalPaymentGatewayFactory : PaymentGatewayFactory
+    {
+        public override IPaymentGateway CreatePaymentGateway()
+        {
+            return new PaypalPaymentGateway();
+        }
+    }
+
+    public class BitcoinPaymentGatewayFactory : PaymentGatewayFactory
+    {
+        public override IPaymentGateway CreatePaymentGateway()
+        {
+            return new BitcoinPaymentGateway();
+        }
+    }
+
+    //Client Code
+    public class ECommercePlatform
+    {
+        public void Checkout(PaymentGatewayFactory factory, decimal amount)
+        {
+            IPaymentGateway paymentGateway = factory.CreatePaymentGateway();
+            paymentGateway.ProcessPayment(amount);
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var platform = new ECommercePlatform();
+
+            platform.Checkout(new CreditCardPaymentGatewayFactory(), 100.75M);
+
+            platform.Checkout(new PaypalPaymentGatewayFactory(), 150.50M);
+
+            platform.Checkout(new BitcoinPaymentGatewayFactory(), 50.30M);
+
+            Console.ReadKey();
         }
     }
 }
